30a59e897d5de2a4265c780a40a780eec417a219
Mark Ackerman <124592505+marKEYfrfx@users.noreply.github.com>
2025-01-07 18:45:40 -0500

    Parsed the children cells; Differentiating betweenv cells before and after a line cell.

--- uml_highlighter.js
+++ uml_highlighter.js
@@ -17,6 +17,7 @@ Draw.loadPlugin(function(editorUi)
 		try
 		{
 			let class_names = new Array();
+			let class_fields = new Array();
 			for (var id in model.cells)
 			{
 				var cell = model.cells[id];
@@ -32,10 +33,31 @@ Draw.loadPlugin(function(editorUi)
 			console.log(class_names)
 			class_names.forEach(function(class_cell)
 				{
-					class_cell.style = class_cell.style + "fillColor=#f8cecc;strokeColor=#b85450;"
+					class_cell.style = "swimlane;fontStyle=1;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeParentMax=0;resizeLast=0;collapsible=1;marginBottom=0;whiteSpace=wrap;html=1;fontFamily=Courier New;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;"
 					model.setValue(class_cell, graph.getLabel(class_cell));
+					class_fields.push(class_cell.children)
 				}
 			)
+			console.log(class_fields)
+			class_fields.forEach(function(field_set)
+			{
+				if(field_set != null)
+					{
+						let field_style = "text;strokeColor=#9673a6;fillColor=#e1d5e7;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;whiteSpace=wrap;html=1;fontFamily=Times New Roman;fontStyle=0"
+						field_set.forEach(function(field)
+						{
+							if(field.style.includes("line"))
+								{
+									field_style = "text;strokeColor=#82b366;fillColor=#d5e8d4;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;whiteSpace=wrap;html=1;fontFamily=Times New Roman;fontStyle=2;"
+								} else {
+									field.style = field_style;
+								}
+								model.setValue(field, graph.getLabel(field));
+						}
+					)
+						}
+				}
+		)
 		}
 		finally
 		{
