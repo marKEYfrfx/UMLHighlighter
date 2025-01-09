129a57e4fc4e71784147c067a03bf292f2258462
Mark <markeyhackerman@gmail.com>
2025-01-09 06:17:45 -0500

    Moved parsing of different cell types into their own functions. Started working on parsing tokens.

--- uml_highlighter.js
+++ uml_highlighter.js
@@ -1,78 +1,91 @@
+const cellRegex = /<[^>]*>|[^< >,!?+-:\(\)]+|[<>,!?+-:\(\)]/g;
+const beforeVar = [
+    "-",
+    "<font face=\"Helvetica\" color=\"#3d0dff\">",
+    "<b>",
+    "<i>"
+]
+const aferVar = [
+    "</i>",
+    "</b>",
+    "</font>"
+]
+const beforeType = [
+    "<font face=\"Courier New\">",
+    "<b>"
+]
+const aferType = [
+    "</b>",
+    "</font>"
+]
+
+
+function parseMethod(methodCell) {
+	let methodTokens = methodCell.value.match(cellRegex).filter(token => token.charAt(0) != '<');
+	let scopeIdx = methodTokens.findIndex(token => token == "+" || token == "-");
+}
+
+function parseField(fieldCell) {
+	console.log(fieldCell.value.match(cellRegex))
+	let fieldTokens = fieldCell.value.match(cellRegex).filter(token => token.charAt(0) != '<');
+	let scopeIdx = fieldTokens.findIndex(token => token == "+" || token == "-");
+	let colonIdx = fieldTokens.findIndex(token => token == ":"); 
+	
+}
+
+function parseClassMembers(memberCells) {
+	if (memberCells != null) {
+		let memberFunc = (cell) => parseField(cell);
+		memberCells.forEach(function (member) {
+			if (member.style.includes("line")) {
+				memberFunc = (cell) => parseMethod(cell);
+			} else {
+				memberFunc(member)
+			}
+		}
+		)
+	}
+}
+
+function parseClassCell(classCell) {
+	parseClassMembers(classCell.children)
+}
+
 /**
  * Explore plugin.
  */
-Draw.loadPlugin(function(editorUi)
-{
-	
+Draw.loadPlugin(function (editorUi) {
+
 	// Adds resource for action
 	mxResources.parse('UMLHighlighter=UML Highlighter');
 
 	// Adds action
-	editorUi.actions.addAction('UMLHighlighter', function()
-	{
+	editorUi.actions.addAction('UMLHighlighter', function () {
 		var graph = editorUi.editor.graph;
 		var model = graph.model;
-		
+		var root = model.getCell(1); //Don't know why but root sin't actually root?!??!
 		model.beginUpdate();
-		try
-		{
-			let class_names = new Array();
-			let class_fields = new Array();
-			for (var id in model.cells)
-			{
-				var cell = model.cells[id];
-				let cell_style = cell.style;
-				if (cell_style != null)
-				{
-					if (cell_style.includes("childLayout"))
-						{
-							class_names.push(cell)
-						}
+		try {
+			root.children.forEach(function (cell) {
+				if (cell != null) {
+					if (cell.style.includes("childLayout")) {
+						parseClassCell(cell)
+					}
 				}
-			}
-			console.log(class_names)
-			class_names.forEach(function(class_cell)
-				{
-					class_cell.style = "swimlane;fontStyle=1;align=center;verticalAlign=top;childLayout=stackLayout;horizontal=1;startSize=26;horizontalStack=0;resizeParent=1;resizeParentMax=0;resizeLast=0;collapsible=1;marginBottom=0;whiteSpace=wrap;html=1;fontFamily=Courier New;fillColor=#f5f5f5;fontColor=#333333;strokeColor=#666666;"
-					model.setValue(class_cell, graph.getLabel(class_cell));
-					class_fields.push(class_cell.children)
-				}
-			)
-			console.log(class_fields)
-			class_fields.forEach(function(field_set)
-			{
-				if(field_set != null)
-					{
-						let field_style = "text;strokeColor=#9673a6;fillColor=#e1d5e7;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;whiteSpace=wrap;html=1;fontFamily=Times New Roman;fontStyle=0"
-						field_set.forEach(function(field)
-						{
-							if(field.style.includes("line"))
-								{
-									field_style = "text;strokeColor=#82b366;fillColor=#d5e8d4;align=left;verticalAlign=top;spacingLeft=4;spacingRight=4;overflow=hidden;rotatable=0;points=[[0,0.5],[1,0.5]];portConstraint=eastwest;whiteSpace=wrap;html=1;fontFamily=Times New Roman;fontStyle=2;"
-								} else {
-									field.style = field_style;
-								}
-								model.setValue(field, graph.getLabel(field));
-						}
-					)
-						}
-				}
-		)
+			})
 		}
-		finally
-		{
+		finally {
 			model.endUpdate();
 		}
 		graph.refresh();
 	});
-	
+
 	var menu = editorUi.menus.get('extras');
 	var oldFunct = menu.funct;
-	
-	menu.funct = function(menu, parent)
-	{
+
+	menu.funct = function (menu, parent) {
 		oldFunct.apply(this, arguments);
-		
+
 		editorUi.menus.addMenuItems(menu, ['-', 'UMLHighlighter'], parent);
 	};
 
