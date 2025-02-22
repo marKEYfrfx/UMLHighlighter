f1f285d08323a550c25a6c69ffe69c5f8076b8f6
Mark Ackerman <124592505+marKEYfrfx@users.noreply.github.com>
2025-02-22 07:23:59 -0500

    Trying to emulate project structure of drawio_mermaid_plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,28 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  mode: 'development',       // or 'production'
-  entry: './src/uml_highlighter.ts', // This is your only "entry" file
-  output: {
-    filename: 'bundle.js',          // The compiled output
-    path: path.resolve(__dirname, 'dist'), 
-    // In dev-server, the file is served from memory at /bundle.js by default
-  },
   module: {
     rules: [
       {
-        test: /\.ts$/, 
-        loader: 'ts-loader',  // or other TS loader
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-      }
-    ]
+      },
+    ],
   },
+  target: 'node',
+  entry: './src/vscode_extension.ts',
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [ '.tsx', '.ts', '.js' ],
   },
-  devServer: {
-    // By default, the dev server will serve your bundle at http://localhost:8080/bundle.js
-    // If you want to serve a static folder, you can specify it here:
-    static: {
-       directory: path.join(__dirname, 'public'),
-     },
-    port: 8080,
+  externals: {
+    vscode: 'commonjs vscode'
   },
+  output: {
+    filename: 'vscode_extension.js',
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist'),
+    devtoolModuleFilenameTemplate: '[resource-path]',
+  },
+  devtool: 'source-map',
 };
