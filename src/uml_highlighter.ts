eb8e27c04c156e61e86f651ae3a3b9c0e548bd1a
Mark Ackerman <124592505+marKEYfrfx@users.noreply.github.com>
2025-03-02 14:18:08 -0500

    Plugin now written in TypeScript and fully back to js implementation functionality. Still very basic. Properly built with scripts.

--- src/uml_highlighter.ts
+++ src/uml_highlighter.ts
@@ -8,7 +8,12 @@
 // Extend the Cell interface to include properties we use in our plugin.
 
 Draw.loadPlugin((editorUi) => {
-    editorUi.parseXml("UMLHighlighter=UML Highlighter");
+    const view : DrawioUI = editorUi;
+    const editor : DrawioEditor = view.editor;
+    const graph : DrawioGraph = editor.graph;
+    const model : DrawioGraphModel = graph.model;
+
+    mxResources.parse("UMLHighlighter=UML Highlighter");
 
     const cellRegex =
       /<[^>]*>|[a-zA-Z0-9]+\[[^\]]*\]|[^< >,!?+\-:\(\)]+|[<>,!?+\-:\(\)]/g;
@@ -18,98 +23,96 @@ Draw.loadPlugin((editorUi) => {
     const beforeType = ['<font face="Courier New">', '<b>', '<font style="color: rgb(0,0,0);">'];
     const aferType = ['</font>', '</b>', '</font>'];
 
-    function parseMethod(methodCell: Cell): void {
-        const graph: max.Graph | any = editorUi.editor.graph;
-        const model: max.GraphDataModel = graph.model;
-
+    function parseMethod(methodCell: DrawioCell): void {
         const cellLabel : string = graph.getLabel(methodCell);
-        const rawMatches = (cellLabel || "").match(cellRegex) as string[] | null;
-        const methodTokens: string[] = (rawMatches ?? []).filter(
+        console.log(cellLabel);
+        const rawMatches = (cellLabel || "").match(cellRegex) as string[];
+        console.log(rawMatches);
+        const methodTokens: string[] = (rawMatches).filter(
             (t) => t.charAt(0) !== "<"
         );
 
         console.log("methodTokens:" + methodTokens);
         const updatedMethodTokens: string[] = [];
+
         // States for method parsing.
         const states: string[] = ["q0", "q1", "q2", "q3", "q4", "q5", "q6"];
         let curState: string = states[0];
 
-        while (methodTokens.length > 0) {
-            let curToken: string = methodTokens.shift() as string;
-
-            switch (curState) {
-                case states[0]:
-                    if (curToken === "(") {
-                        curState = states[1];
-                    } else {
+        methodTokens.forEach((curToken : string) => {
+			switch (curState) {
+				case states[0]:
+					if (curToken === "(") {
                         updatedMethodTokens.push(curToken);
+						curState = states[1];
+					} else {
+						updatedMethodTokens.push(curToken);
                         updatedMethodTokens.push(" ");
-                        curToken = methodTokens.shift() as string;
-                    }
-                    updatedMethodTokens.push(curToken);
-                    break;
-
-                case states[1]:
-                    if (curToken === ")") {
-                        curState = states[3];
-                        updatedMethodTokens.push(curToken);
-                    } else {
-                        updatedMethodTokens.push(curToken);
-                        curState = states[2];
-                    }
-                    break;
-
-                case states[2]:
-                    if (curToken === ",") {
-                        curState = states[6];
-                        updatedMethodTokens.push(curToken);
-                        updatedMethodTokens.push(" ");
-                    } else if (curToken === ")") {
-                        curState = states[3];
-                        updatedMethodTokens.push(curToken);
-                    } else {
-                        updatedMethodTokens.push(" ");
-                        updatedMethodTokens.push(curToken);
-                        curState = states[5];
-                    }
-                    break;
-
-                case states[3]:
-                    curState = states[4];
-                    updatedMethodTokens.push(curToken);
-                    break;
-
-                case states[4]:
-                    updatedMethodTokens.push(" ");
-                    updatedMethodTokens.push(curToken);
-                    break;
-
-                case states[5]:
-                    if (curToken === ",") {
-                        curState = states[6];
-                    } else if (curToken === ")") {
-                        curState = states[3];
-                    }
-                    updatedMethodTokens.push(curToken);
-                    break;
-
-                case states[6]:
-                    if (curToken === ")") {
-                        curState = states[3];
-                    }
-                    updatedMethodTokens.push(curToken);
-                    curState = states[2];
-                    break;
-            }
-        }
+					}
+					break;
+				case states[1]:
+					if (curToken === ")") {
+						curState = states[3];
+						updatedMethodTokens.push(curToken);
+					} else {
+						updatedMethodTokens.splice(updatedMethodTokens.length, 0, ...beforeType);
+						updatedMethodTokens.push(curToken);
+						updatedMethodTokens.splice(updatedMethodTokens.length, 0, ...aferType);
+						curState = states[2];
+					}
+					break;
+				case states[2]:
+					if (curToken === ",") {
+						curState = states[6];
+						updatedMethodTokens.push(curToken)
+						updatedMethodTokens.push(" ")
+					} else if (curToken === ")"){
+						curState = states[3];
+						updatedMethodTokens.push(curToken)
+					}
+					else {
+						updatedMethodTokens.push(" ")
+						updatedMethodTokens.splice(updatedMethodTokens.length, 0, ...beforeVar);
+						updatedMethodTokens.push(curToken);
+						updatedMethodTokens.splice(updatedMethodTokens.length, 0, ...aferVar);
+						curState = states[5];
+					}
+					break;
+				case states[3]:
+					curState = states[4]
+					updatedMethodTokens.push(curToken)
+					break;
+				case states[4]:
+					updatedMethodTokens.push(" ");
+					updatedMethodTokens.splice(updatedMethodTokens.length, 0, ...beforeType);
+					updatedMethodTokens.push(curToken);
+					updatedMethodTokens.splice(updatedMethodTokens.length, 0, ...aferType);
+					break;
+				case states[5]:
+					if (curToken === ",") {
+						curState = states[6];
+					} else if (curToken === ")") {
+						curState = states[3];
+					}
+					updatedMethodTokens.push(curToken)
+					break;
+				case states[6]:
+					if (curToken === ")") {
+						curState = states[3]
+					}
+					updatedMethodTokens.splice(updatedMethodTokens.length, 0, ...beforeType);
+					updatedMethodTokens.push(curToken);
+					updatedMethodTokens.splice(updatedMethodTokens.length, 0, ...aferType);
+					curState = states[2]
+					break;
+			}
+		});
+        console.log(updatedMethodTokens);
         // Update the cell value with the new tokens.
         model.setValue(methodCell, updatedMethodTokens.join(""));
     }
 
-    function parseField(fieldCell: max.Cell): void {
-        const graph : any = editorUi.editor.graph;
-        const model : max.GraphDataModel = graph.model;
-
+    function parseField(fieldCell: DrawioCell): void {
         const fieldTokens = (fieldCell.value?.match(cellRegex) || []).filter(
             (token: string) => token.charAt(0) !== "<"
         );
@@ -127,51 +130,43 @@ Draw.loadPlugin((editorUi) => {
         model.setValue(fieldCell, fieldTokens.join(" "));
     }
 
-    function parseClassMembers(memberCells: max.Cell[]): void {
-        const graph = editorUi.editor.graph;
-        const model = graph.model;
-
-        if (memberCells != null) {
-            memberCells.forEach((member: max.Cell) => {
-                let memStyle : max.CellStyle = member.getStyle();
-                let styles : string = memStyle.baseStyleNames;
-                if (styles.includes('childLayout')) {
-                    parseClassMembers(member.children || []);
-                } else if (memStyle.style("line")) {
-                    parseMethod(member);
-                } else {
-                    parseField(member);
-                    let tempStyle = member.style;
-                    if (tempStyle.includes("autosize")) {
-                        model.setStyle(
-                            member,
-                            tempStyle.replace("autosize=0", "autosize=1")
-                        );
-                    } else {
-                        if (!tempStyle.endsWith(";")) {
-                            tempStyle += ";";
-                        }
-                        model.setStyle(member, tempStyle + "autosize=1;");
-                    }
-                }
-            });
-        }
-    }
+	function parseClassMembers(memberCells : DrawioCell[]) {
+		if (memberCells != null) {
+			let memberFunc = (cell : DrawioCell) => parseField(cell);
+			memberCells.forEach(function (member) {
+				if (member.style.includes("line")) {
+					memberFunc = (cell) => parseMethod(cell);
+				} else {
+					memberFunc(member);
+					var tempStyle = member.style;
+					if (tempStyle.includes("autosize")){
+						model.setStyle(member, tempStyle.replace("autosize=0", "autosize=1"));
+					}
+					if (tempStyle.endsWith(';')){
+						model.setStyle(member, tempStyle.concat("autosize=1;"));
+					} else {
+						model.setStyle(member, tempStyle.concat(";autosize=1;"));
+					}
+				}
+			}
+			)
+		}
+	}
 
     editorUi.actions.addAction("UMLHighlighter", () => {
-        const graph: Graph = editorUi.editor.graph;
-        const model: GraphModel = graph.model;
-        const root: Cell = model.cells["1"] as Cell;
+        const root: DrawioCell = model.cells["1"] as DrawioCell;
 
         model.beginUpdate();
         console.log("update");
         try {
             if (root && root.children) {
-                root.children.forEach((cell: Cell) => {
+                root.children.forEach((cell: DrawioCell) => {
                     if (cell.style.includes("stackLayout")) {
                         parseClassMembers(cell.children || []);
                     }
                 });
+                graph.refresh();
+                graph.autoSizeCell(root, true);
             }
         } finally {
             model.endUpdate();
